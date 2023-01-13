7018c2258b8ecb5cfa79049d41d68bec4896acd0
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-01-13 18:54:53 -0300

    github section

--- javascript/index.js
+++ javascript/index.js
@@ -19,3 +19,37 @@ $('.owl-carousel').owlCarousel({
     }
   }
 });
+
+// GitHub informações
+
+$(document).ready(function(){
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      let response = JSON.parse(xmlHttp.responseText);
+
+      let spinner = document.getElementById('spinner');
+      spinner.classList.add('d-none');
+
+      let account = document.getElementById('conta');
+      let picture = account.getElementsByClassName('picture')[0];
+      picture.src = response.avatar_url;
+      let username = account.getElementsByClassName('username')[0];
+      username.innerText = `@${response.login}`;
+      username.href = response.html_url;
+      let name = account.getElementsByClassName('name')[0];
+      name.innerText = response.name;
+      let bio = account.getElementsByClassName('bio')[0];
+      bio.innerText = response.bio;
+      let dateContent = new Date(response.created_at);
+      let date = account.getElementsByClassName('date')[0];
+      date.innerText = dateContent.toLocaleDateString();
+      account.classList.remove('d-none');
+      account.classList.add('d-flex');
+    }
+  };
+  let username = 'kevendasilva';
+  let theUrl = `https://api.github.com/users/${username}`;
+  xmlHttp.open('GET', theUrl, true);
+  xmlHttp.send(null);
+});
